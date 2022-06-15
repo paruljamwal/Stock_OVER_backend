f3ceed7074a87443cbd1a8fa49e057f27fc0905a
paruljamwal <paruljamwal2000@gmail.com>
2022-06-15 09:32:48 +0530

    submission

--- index.js
+++ index.js
@@ -124,12 +124,70 @@ app.patch("/products/:id", async (req, res) => {
 
 //5.delete
 
-app.delete("/products/:id", async (req, res) => {
+//search**********************
+
+//1. get
+
+app.get("/search", async (req, res) => {
   try {
-    const product = await Product.findByIdAndDelete(req.params.id)
+    const search = await Search.find().lean().exec();
+    return res.status(200).send({search});
+  } catch (error) {
+    return res
+      .status(500)
+      .send({ message: "Something went wrong try again later" });
+  }
+});
+
+//2.post
+
+app.post("/search", async (req, res) => {
+  try {
+    const search = await Search.create(req.body);
+    return res.status(200).send({search});
+  } catch (error) {
+    return res
+      .status(500)
+      .send({ message: "Something went wrong try again later" });
+  }
+});
+
+//3. get single
+
+app.get("/search/:name", async (req, res) => {
+  try {
+    const search = await Search.findOne(req.query.name).lean().exec();
+    return res.status(200).send(search);
+  } catch (error) {
+    return res
+      .status(500)
+      .send({ message: "Something went wrong try again later" });
+  }
+});
+
+//4.patch
+
+app.patch("/search/:id", async (req, res) => {
+  try {
+    const search = await Search.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    })
       .lean()
       .exec();
-    return res.status(200).send(products);
+    return res.status(200).send({search});
+  } catch (error) {
+    return res
+      .status(500)
+      .send({ message: "Something went wrong try again later" });
+  }
+});
+
+app.delete("/search/:id", async (req, res) => {
+  try {
+    const search = await Search.findByIdAndDelete(req.params.id)
+      .lean()
+      .exec();
+    return res.status(200).send(search);
   } catch (error) {
     return res
       .status(500)
